4e85c4be8c7b8f129e8cb1b6fea193876ef0c3b0
moshams272 <mohamedshamseldeen95@gmail.com>
2026-02-27 17:48:19 +0200

    implement dynamic versioning for generated API docs

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ const path = require("path");
 const parseDocs = require("./parser.js");
 const generateMDX = require("./generate.js");
 
+const packageJsonPath = path.join(__dirname, "../package.json");
+const packageData = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+
+const majorVersion = packageData.version.split(".")[0]; 
+const versionFolder = `v${majorVersion}`;
+
 const jsonPath = path.join(__dirname, "../output.json"); 
 const docs = parseDocs(jsonPath);
 
-const outputDir = path.join(__dirname, "../docs");
+const outputDir = path.join(__dirname, `../docs/${versionFolder}/api`);
 
 if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir);
+  fs.mkdirSync(outputDir, { recursive: true });
 }
 
 docs.forEach(doc => {
